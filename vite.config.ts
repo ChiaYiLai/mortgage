ad87b5d227ffa564abfeb50d5e7d48e191f9094c
ChiaYiLai <cheesebe@gmail.com>
2025-04-22 13:37:28 +0800

    fix: settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import prerender from "vite-plugin-prerender";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/mortgage/",
-  plugins: [
-    vue(),
-    prerender({
-      staticDir: "dist",
-      routes: ["/"], // 想要 pre-render 的路徑
-    }),
-  ],
+  plugins: [vue()],
   server: {
     host: true,
     port: 5176,
